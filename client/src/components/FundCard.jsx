03275073d75453b2fe7ab10be7a8db4d2126bd65
Hayaatunishaa35 <hayatunisha1@gmail.com>
2023-07-15 19:00:24 +0530

    first commit

---
+++ client/src/components/FundCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+import { tagType, thirdweb } from '../assets';
+import { daysLeft } from '../utils';
+
+const FundCard = ({ owner, title, description, target, deadline, amountCollected, image, handleClick }) => {
+  const remainingDays = daysLeft(deadline);
+  
+  return (
+    <div className="sm:w-[288px] w-full rounded-[15px] bg-[#f8e6f8] cursor-pointer" onClick={handleClick}>
+      <img src={image} alt="fund" className="w-full h-[158px] object-cover rounded-[15px]"/>
+
+      <div className="flex flex-col p-4">
+
+        <div className="block">
+          <h3 className="font-epilogue font-semibold text-[16px] text-black text-left leading-[26px] truncate">{title}</h3>
+          <p className="mt-[5px] font-epilogue font-normal text-[#565763] text-left leading-[18px] truncate">{description}</p>
+        </div>
+
+        <div className="flex justify-between flex-wrap mt-[15px] gap-2">
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#898a94] leading-[22px]">{amountCollected}</h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#717280] sm:max-w-[120px] truncate">Raised of {target}</p>
+          </div>
+          <div className="flex flex-col">
+            <h4 className="font-epilogue font-semibold text-[14px] text-[#6e6f79] leading-[22px]">{remainingDays}</h4>
+            <p className="mt-[3px] font-epilogue font-normal text-[12px] leading-[18px] text-[#4c4d57] sm:max-w-[120px] truncate">Days Left</p>
+          </div>
+        </div>
+
+        <div className="flex items-center mt-[20px] gap-[12px]">
+          <div className="w-[30px] h-[30px] rounded-full flex justify-center items-center bg-[#e8b8e8]">
+            <img src={thirdweb} alt="user" className="w-1/2 h-1/2 object-contain"/>
+          </div>
+          <p className="flex-1 font-epilogue font-normal text-[12px] text-[#5c5d68] truncate">by <span className="text-[#888992]">{owner}</span></p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FundCard
---
+++ client/src/constants/index.js
@@ -0,0 +1,22 @@
+import { createCampaign, dashboard, logout, payment, profile, withdraw } from '../assets';
+
+export const navlinks = [
+  {
+    name: 'Home',
+    imgUrl: dashboard,
+    link: '/',
+    id: 0
+  },
+  {
+    name: 'Start Campaign',
+    imgUrl: createCampaign,
+    link: '/create-campaign',
+    id: 1
+  },
+  {
+    name: 'Profile',
+    imgUrl: profile,
+    link: '/profile',
+    id: 4
+  },
+];
